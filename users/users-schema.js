235d4459f2b53547da8e80573a4998d5064e015c
Rachel Dao <rachelqdao@gmail.com>
2022-12-08 19:19:31 -0500

    updated users schema

--- users/users-schema.js
+++ users/users-schema.js
@@ -4,7 +4,7 @@ const usersSchema = mongoose.Schema({
     username: {type: String, required: true, unique: true},
     password: {type: String, required: true},
     email: String,
-    userType: {type: String, enum: ['USER', 'AUTHOR', 'ADMIN']},
+    userType: {type: String, enum: ['USER', 'ADMIN', 'BOOK CLUB OWNER']},
     firstName: String,
     lastName: String,
     dateJoined: Date,
